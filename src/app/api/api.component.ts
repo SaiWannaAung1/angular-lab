3a7ecffb70a782a18b2bfc1fde2b2e20195ca93c
SaiWannaAung1 <saiwannaaung095@gmail.com>
2025-02-09 23:31:29 +0000

    create api component

---
+++ src/app/api/api.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-api',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './api.component.html',
+  styleUrl: './api.component.scss'
+})
+export class ApiComponent {
+
+}
