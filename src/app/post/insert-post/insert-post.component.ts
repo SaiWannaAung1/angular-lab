134e46d2ca23723fd693d31e8a558846254b4d72
SaiWannaAung1 <saiwannaaung095@gmail.com>
2025-02-10 23:57:53 +0000

    add ng form

--- src/app/post/insert-post/insert-post.component.ts
+++ src/app/post/insert-post/insert-post.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-insert-post',
   templateUrl: './insert-post.component.html',
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ],
   styleUrl: './insert-post.component.scss'
 })
 export class InsertPostComponent {
-
+  submitForm(userForm:any ) {
+    console.log(userForm.value); // Handle form submission
+  }
 }
