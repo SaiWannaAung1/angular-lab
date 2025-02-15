1f38c1025f83f6932bce7075c53031ca54bd8b3c
SaiWannaAung1 <saiwannaaung095@gmail.com>
2025-02-15 23:45:23 +0000

    route fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-;
 import {AppComponent} from './app.component';
 import {PostComponent} from './components/post/post.component';
 import {ViewPostComponent} from './components/post/view-post/view-post.component';
 import {InsertPostComponent} from './components/post/insert-post/insert-post.component';
+import {ReactiveFormComponent} from './components/reactive-form/reactive-form.component';
 
 
 
@@ -11,12 +11,12 @@ export const routes: Routes = [
   {
     path: '', component: PostComponent,
     children: [
-      {path: 'view-post',     component: ViewPostComponent
-      },
+      {path: 'view-post',     component: ViewPostComponent},
       // {path: 'view-post',     loadComponent: () => import('./post/view-post/view-post.component').then(c => c.ViewPostComponent)
       // },
       {path: 'insert-post', component: InsertPostComponent},
       ]
   },
+  {path: 'reactiveForm-vald', component: ReactiveFormComponent},
   {path: 'post', component: AppComponent},
 ];
