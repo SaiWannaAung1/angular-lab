3e06e56858b92866e3a26abda5d2064b2e3898e6
SaiWannaAung1 <saiwannaaung095@gmail.com>
2025-02-15 23:38:17 +0000

    reactive form

--- src/app/components/reactive-form/reactive-form.component.ts
+++ src/app/components/reactive-form/reactive-form.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import {MenuComponent} from '../menu/menu.component';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
+  imports: [
+    ReactiveFormsModule
+  ],
   styleUrl: './reactive-form.component.scss'
 })
 export class ReactiveFormComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Data:', this.loginForm.value);
+    }
+  }
 }
